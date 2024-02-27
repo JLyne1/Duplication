069366439805fd736dd85dd877c13eb721f96f52
JoeLyne <josephlyne16@yahoo.co.uk>
2024-02-27 18:15:45 +0000

    Resolution changes
    Removal of resolution features

--- Assets/Scripts/Menus/OptionsMenu.cs
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -5,41 +5,7 @@ using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
-    [SerializeField] private TMP_Dropdown resolutionDropdown;
     [SerializeField] private AudioMixer audioMixer;
-
-    Resolution[] resolutions;
-
-    private void Start() 
-    {
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new();
-        
-        int currentResolutionIndex = 0;
-        for(int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width && 
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-    }
-
-    public void ResolutionControl (int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-    }
 
     public void FullscreenControl(bool isFullscreen)
     {
